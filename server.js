707008ae1246ed21819d9689130d1c30da539fe7
Holger Desktop <hirmo.holger@gmail.com>
2020-04-04 21:38:50 +0300

    barrack fasterr

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-mongoose.connect('mongodb+srv://obamachair:eCqbW4T51YXEbk8H@cluster0-i12e4.mongodb.net/todolistDB', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost:27017/todolistDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://obamachair:4p5HIpjFJ8DTv0RoH@cluster0-i12e4.mongodb.net/todolistDB', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb+srv://obamachair:4p5HIpjFJ8DTv0Ro@cluster0-i12e4.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const itemSchema = {
     name: String
